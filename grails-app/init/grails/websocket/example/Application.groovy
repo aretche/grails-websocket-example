7721959cd6b600a3821044c8fc1828870bc2f6a0
Gabriel Arellano <arellanog@cent.gov.ar>
2017-10-31 08:52:03 -0300

    Agrego aplicación Grails 3.3.1 vacía.

---
+++ grails-app/init/grails/websocket/example/Application.groovy
@@ -0,0 +1,10 @@
+package grails.websocket.example
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration {
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}
---
+++ grails-app/init/grails/websocket/example/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails.websocket.example
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
